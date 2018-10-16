eac769581401dbd8b58e41eb48c84792ea2941d7
George Karagiorgos <georgekar94@gmail.com>
2018-10-16 21:57:26 +0300

    Removed hidden .gitignore in client folder, moved rules to the main .gitignore.

--- Client/.eslintrc.js
+++ Client/.eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-  'extends': 'plugin:vue/strongly-recommended'
-}
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+  },
+
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+
+  'extends': [
+    'plugin:vue/strongly-recommended',
+    '@vue/airbnb'
+  ]
+};
